f4fda6a3d52951021d028010901f0254cfef70d6
tonesofheresy <86.s.mittal@gmail.com>
2013-09-13 17:21:44 -0700

    Delete config.php

--- .private/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "brass");
-defined('DB_PASS')   ? null : define("DB_PASS", "m2Liyx5cxX]x");
-defined('DB_NAME')   ? null : define("DB_NAME", "brass_offerings");
